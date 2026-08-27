05930789836e7ade4c6d62c478bd3271458167ac
Miraan Ejaz <miraanejaz1@gmail.com>
2026-08-27 15:24:20 +0000

    Add functions for showing, adding and removing tasks

--- todo.py
+++ todo.py
@@ -1,16 +1,30 @@
+tasks = ["Buy milk", "Call num", "Finish python lesson"]
 
-tasks = ["Buy milk", "Call num", "Finish python learning"]
+def show_tasks():
+    print("TODO-LIST")
+    print("----------")
+    for index,task in enumerate(tasks, start=1):
+        print(f"{index}, {task}")
+    print("----------")
+    print(f"You have {len(tasks)} tasks \n")
 
-print("My To-Do-List")
+def add_task(task):
+    tasks.append(task)
 
-print("------------")
+def remove_task(task):
+    tasks.remove(task)    
 
-for index, value in enumerate(tasks, start = 1):
-    print(f"{index}. {value}")
-
-
-print("-----------")
-print(f"Total number of tasks: {len(tasks)}")
+def count_tasks():
+    return len(tasks)
 
 
-    
+show_tasks()
+
+add_task("Water plants")
+show_tasks()
+
+remove_task("Call num")
+show_tasks()
+
+total = count_tasks()
+print(f"Total tasks: {total}")
